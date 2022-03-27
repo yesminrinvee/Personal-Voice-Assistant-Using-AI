4db6fbd2172ec51ac5e8e02032c73ea598d87986
C19153_ Yesmin <89390901+yesminrinvee@users.noreply.github.com>
2022-03-27 19:44:22 +0600

    Create voiceapp.py

---
+++ voiceapp.py
@@ -0,0 +1,12 @@
+import sys
+import webbrowser
+
+import speech_recognition as sr
+import pyttsx3
+# import pyjokes
+import os
+import datetime
+# import wikipedia
+import multiprocessing
+import playsound
+from gtts import gTTS
